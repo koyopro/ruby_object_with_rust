5a640ec5504f218d7f9a3a5d38fb53c602790906
koyopro <koyopro2.0@gmail.com>
2020-12-23 02:51:10 +0900

    refactor

--- spec/ruby_object_with_rust/user_spec.rb
+++ spec/ruby_object_with_rust/user_spec.rb
@@ -1,8 +1,7 @@
 RSpec.describe RubyObjectWithRust::User do
-  it do
-    u = described_class.new(4)
-    expect(u.display).to eq('id: 4, name: ')
-    u.name = 'piyo'
-    expect(u.display).to eq('id: 4, name: piyo')
-  end
+  let(:user) { described_class.new(4) }
+
+  before { u.name = 'piyo'}
+
+  it { expect(u.display).to eq('id: 4, name: piyo') }
 end
--- spec/ruby_object_with_rust_spec.rb
+++ spec/ruby_object_with_rust_spec.rb
@@ -1,15 +1,2 @@
 RSpec.describe RubyObjectWithRust do
-  it "has a version number" do
-    expect(RubyObjectWithRust::VERSION).not_to be nil
-  end
-
-  describe '.create_user' do
-    it do
-      u = RubyObjectWithRust.create_user(2)
-      RubyObjectWithRust.set_name(u, 'piyo')
-      display = RubyObjectWithRust.get_display(u)
-      expect(display).to eq('id: 2, name: piyo')
-      RubyObjectWithRust.rust_free(u)
-    end
-  end
 end
